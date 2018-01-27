00307fdd52d17c81502dbe9653bf4339db406758
Alex117 <enola_enola2003@yahoo.com>
2018-01-27 20:14:10 +0200

    Initial commit

---
+++ app/src/main/java/com/alexandrud/atreia/FragmentTwo.java
@@ -0,0 +1,28 @@
+package com.alexandrud.atreia;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by user on 1/24/2018.
+ */
+
+public class FragmentTwo extends Fragment {
+
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_two, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
